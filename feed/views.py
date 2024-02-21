ad623d220b1e5bcb6a0e3592d9cdb7e1c8e52dd1
Mohammed Nihal <nihalcrixcrox56@gmail.com>
2024-02-21 14:54:30 +0530

    testing out

---
+++ feed/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import tweet
+# Create your views here.
+
+
+def home(request):
+    context={'tweets':tweet.objects.all}
+    return render(request,'feed/home.html')
